0ff8da868005338925ce057be69e4c9801f3d515
root <soumojyotiganguly270@gmail.com>
2020-07-26 23:36:02 +0530

    Map in Kt

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -1,8 +1,13 @@
+import java.util.*
+
 fun main(args: Array<String>) {
 
-    var numb = listOf(1, 3, 2, 4)
-    for ((i, e) in numb.withIndex()) {
-        println("$i : $e")
+    var st = TreeMap<String, Int>()
+    st["Soumo"] = 22
+    st["Ujjwal"] = 23
+
+    for ((name, age) in st) {
+        println("$name : $age")
     }
 }
 
