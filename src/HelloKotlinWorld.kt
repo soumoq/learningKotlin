9903a29f28328221191169fe138054d1a854ef46
root <soumojyotiganguly270@gmail.com>
2020-07-26 19:50:57 +0530

    if else in KT

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -1,6 +1,15 @@
 fun main(args: Array<String>) {
-    var hello = Hello()
-    hello.name = "Soumo"
-    println(hello.name)
+    var num1 = 4
+    var num2 = 5
+    var res = 0
+
+    if (num1 > num2) {
+        println("num1 > num2")
+        res = 1
+    } else {
+        println("num1 < num2")
+        res = 2
+    }
+    println(res)
 }
 
