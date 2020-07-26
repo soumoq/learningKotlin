d782207fe0b83c3ec2245d6a6defbc3138c66d83
root <soumojyotiganguly270@gmail.com>
2020-07-26 20:22:23 +0530

    switch case

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -1,8 +1,20 @@
 fun main(args: Array<String>) {
-    val hello :Hello = Hello()
-    var num1 : Int ?= null
-    var num2 : Int = 8
-    num1 = 5
-    println(num1+num2)
+    val hello: Hello = Hello()
+    var num1: Int? = null
+    var num2: Int = 8
+
+    var res = when (num2) {
+        7 -> {
+            "seven"
+        }
+        8 -> {
+            var res=num2
+            "eight $res"
+        }
+        else -> {
+            "give proper input"
+        }
+    }
+    println("Result is $res")
 }
 
