98dbda7a2dbd96b15d7032d82af38e08d373b667
root <soumojyotiganguly270@gmail.com>
2020-08-01 00:00:17 +0530

    Abstract method in KT

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -3,17 +3,24 @@ import java.util.*
 
 fun main(args: Array<String>) {
     var b = B()
+    b.fun1()
+
 }
 
 abstract class A {
     init {
         println("A")
     }
+    abstract fun fun1()
 }
 
 class B : A()
 {
     init {
         println("B")
+    }
+    override fun fun1()
+    {
+        println("fun 1")
     }
 }
