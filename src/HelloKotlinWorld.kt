beafa677f398e70bcfdf6c7672ffb28af1c01da4
root <soumojyotiganguly270@gmail.com>
2020-07-31 23:55:12 +0530

    constructor in KT

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -2,36 +2,17 @@ import java.lang.*
 import java.util.*
 
 fun main(args: Array<String>) {
-    var th = ImplementThread()
-    th.start()
-    var th1 = Thread(ThreadRunnable())
-    th1.start()
+    val a = A(1,2)
 }
 
-class ImplementThread : Thread() {
-    override fun run() {
-        try {
-            println("Inside run")
-            sleep(2000);
-            println("End run")
-        } catch (e: Exception) {
-            println(e)
-        }
+class A {
 
+    init {
+        println("A")
     }
-}
 
-class ThreadRunnable : Runnable {
-    override fun run() {
-
-        try {
-            println("Inside runnable run")
-            Thread.sleep(1000);
-            println("End runnable run")
-        } catch (e: Exception) {
-            println(e)
-        }
-
+    constructor(a: Int, b: Int){
+        println(a+b)
     }
 
 }
