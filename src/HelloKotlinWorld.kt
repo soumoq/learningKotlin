216d29d034e9d4341504ddc7f2a5b77db40d1959
root <soumojyotiganguly270@gmail.com>
2020-07-31 23:43:00 +0530

    thread using runnable interface in kt (bug fix)

--- src/HelloKotlinWorld.kt
+++ src/HelloKotlinWorld.kt
@@ -4,25 +4,34 @@ import java.util.*
 fun main(args: Array<String>) {
     var th = ImplementThread()
     th.start()
-    var rTh = ThreadRunnable()
-    rTh.run();
+    var th1 = Thread(ThreadRunnable())
+    th1.start()
 }
 
-class ImplementThread : Thread()
-{
+class ImplementThread : Thread() {
     override fun run() {
-        println("Inside run")
-        Thread.sleep(2000);
-        println("End run")
+        try {
+            println("Inside run")
+            sleep(2000);
+            println("End run")
+        } catch (e: Exception) {
+            println(e)
+        }
+
     }
 }
 
-class ThreadRunnable : Runnable
-{
+class ThreadRunnable : Runnable {
     override fun run() {
-        println("Inside runnable run")
-        Thread.sleep(1000);
-        println("End runnable run")
+
+        try {
+            println("Inside runnable run")
+            Thread.sleep(1000);
+            println("End runnable run")
+        } catch (e: Exception) {
+            println(e)
+        }
+
     }
 
 }
